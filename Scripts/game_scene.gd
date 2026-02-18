b17ecac82b537a4cbec47bf6d681f3442deb4b12
jordonbigelow <the-music-dude@hotmail.com>
2026-02-18 11:53:39 -0700

    added a 4th level

--- Scripts/game_scene.gd
+++ Scripts/game_scene.gd
@@ -8,9 +8,11 @@ signal game_over
 @onready var level1_scene: PackedScene = preload("res://Scenes/level1.tscn")
 @onready var level2_scene: PackedScene = preload("res://Scenes/level2.tscn")
 @onready var level3_scene: PackedScene = preload("res://Scenes/level3.tscn")
+@onready var level4_scene: PackedScene = preload("res://Scenes/level4.tscn")
 @onready var level1: Node
 @onready var level2: Node
 @onready var level3: Node
+@onready var level4: Node
 @onready var levels: Array[Node]
 @onready var current_level 
 
@@ -18,12 +20,14 @@ func _ready() -> void:
 	level1 = level1_scene.instantiate()
 	level2 = level2_scene.instantiate()
 	level3 = level3_scene.instantiate()
+	level4 = level4_scene.instantiate()
 	
 	current_level = level1
 	
 	levels.append(level1)
 	levels.append(level2)
 	levels.append(level3)
+	levels.append(level4)
 	
 	for level in levels:
 		level.game_over.connect(_on_game_over)
@@ -46,7 +50,6 @@ func _on_level_ball_entered_killzone() -> void:
 
 func _on_level_ball_broke_out(score: int) -> void:
 	prints("Score:", score)
-	#get_tree().change_scene_to_packed(level2_scene)
 	_switch_level(current_level)
 
 func _switch_level(level: Node) -> void:
@@ -61,4 +64,9 @@ func _switch_level(level: Node) -> void:
 		add_child(current_level)
 		#get_tree().change_scene_to_packed(level3_scene)
 	elif level == level3:
+		remove_child(current_level)
+		current_level = level4
+		add_child(current_level)
+		#get_tree().change_scene_to_packed(level4_scene)
+	elif level == level4:
 		get_tree().change_scene_to_packed(main_menu_scene)
