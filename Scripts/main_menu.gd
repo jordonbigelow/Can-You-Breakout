537b07971c931249fd8097acc3cde21e64b5ce60
jordonbigelow <the-music-dude@hotmail.com>
2026-03-01 10:44:18 -0700

    added sound and music

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,9 +1,16 @@
 extends Control
 
 @onready var game_scene: PackedScene = load("res://Scenes/game_scene.tscn")
+@onready var settings_scene: PackedScene = load("res://Scenes/settings_menu.tscn")
 @onready var click_sound := $SoundEffects
 
 func _on_play_button_pressed() -> void:
 	click_sound.play(0.89)
 	await get_tree().create_timer(0.8).timeout
 	get_tree().change_scene_to_packed(game_scene)
+
+
+func _on_settings_button_pressed() -> void:
+	click_sound.play(0.89)
+	await get_tree().create_timer(0.8).timeout
+	get_tree().change_scene_to_packed(settings_scene)
