6b77c85849fd585fd3a2310d90afcd9aa2180386
jordonbigelow <the-music-dude@hotmail.com>
2026-03-04 08:54:23 -0700

    fixed issue with the paddle controller that would always cause player to move left, and not right

--- Scripts/paddle.gd
+++ Scripts/paddle.gd
@@ -7,10 +7,17 @@ var direction_left: float = -1.0 * speed
 var direction_right: float = 1.0 * speed
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_pressed("move_left"):
-		velocity.x = move_toward(velocity.x, direction_left, slow_down_speed)
-	elif Input.is_action_pressed("move_right"):
-		velocity.x = move_toward(velocity.x, direction_right, slow_down_speed)
+	var input_direction := Input.get_axis("move_left", "move_right")
+	
+	if input_direction != 0:
+		velocity.x = move_toward(velocity.x, input_direction * abs(direction_right), slow_down_speed)
 	else:
 		velocity.x = move_toward(velocity.x, 0.0, slow_down_speed)
+		
+	#if Input.is_action_pressed("move_left"):
+		#velocity.x = move_toward(velocity.x, direction_left, slow_down_speed)
+	#elif Input.is_action_pressed("move_right"):
+		#velocity.x = move_toward(velocity.x, direction_right, slow_down_speed)
+	#else:
+		#velocity.x = move_toward(velocity.x, 0.0, slow_down_speed)
 	move_and_slide()
